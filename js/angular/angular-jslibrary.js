bba4fdf9e8b4acb57518df36a388dad7f1dd2f43
Luiz Carlos <argentinaluiz@gmail.com>
2013-11-27 14:28:41 -0200

    Inclusão jsTooltip

--- js/angular/angular-jslibrary.js
+++ js/angular/angular-jslibrary.js
@@ -5,48 +5,48 @@ var jslibrary = angular.module('jslibrary', []);
  * use data-ng-initial
  */
 var ngInitialDirective = function() {
-        'use strict';
-        return {
-            restrict: 'A',
-            controller: ['$scope', '$attrs', '$parse', function($scope, $attrs, $parse) {
-                    var val = $attrs.ngInitial || $attrs.value,
-                            getter = $parse($attrs.ngModel),
-                            setter = getter.assign;
-                    return setter($scope, val);
-                }]
-        };
+    'use strict';
+    return {
+        restrict: 'A',
+        controller: ['$scope', '$attrs', '$parse', function($scope, $attrs, $parse) {
+                var val = $attrs.ngInitial || $attrs.value,
+                        getter = $parse($attrs.ngModel),
+                        setter = getter.assign;
+                return setter($scope, val);
+            }]
     };
+};
 
 /**
  * Diretivas para redirecionar a pagina atraves de um clique de um botao
  * use data-ng-gotopath
  */
 var ngGotopathDirective = function() {
-        'use strict';
-        return {
-            restrict: 'A',
-            controller: ['$element', '$attrs', '$jsUrl', function($element, $attrs, $jsUrl) {
-                    $element.bind('click', function() {
-                        $jsUrl.goToPath($attrs.ngGotopath);
-                    });
-                }]
-        };
+    'use strict';
+    return {
+        restrict: 'A',
+        controller: ['$element', '$attrs', '$jsUrl', function($element, $attrs, $jsUrl) {
+                $element.bind('click', function() {
+                    $jsUrl.goToPath($attrs.ngGotopath);
+                });
+            }]
     };
+};
 
 var ngMessagemodelDirective = function() {
-        'use strict';
-        return {
-            restrict: 'A',
-            scope: {
-                message: "=ngModel"
-            },
-            controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-                    $scope.$watch($attrs.ngModel, function(value) {
-                        $element.html(value);
-                    });
-                }]
-        };
+    'use strict';
+    return {
+        restrict: 'A',
+        scope: {
+            message: "=ngModel"
+        },
+        controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+                $scope.$watch($attrs.ngModel, function(value) {
+                    $element.html(value);
+                });
+            }]
     };
+};
 
 var jsUrl = function($window) {
     'use strict';
@@ -55,8 +55,29 @@ var jsUrl = function($window) {
     };
 };
 
+var jsTooltip = function($window) {
+    'use strict';
+    this.showTooltip = function(element, text, model) {
+        var el = $(element),
+                scope = angular.element(el).scope();
+        scope[model] = text;
+        $window.setTimeout(function() {
+            el.blur().focus();
+        }, 100);
+    };
+    this.hideTooltip = function(element, model) {
+        var el = $(element),
+                scope = angular.element(el).scope();
+        scope[model] = "";
+        $window.setTimeout(function() {
+            el.blur().focus();
+        }, 100);
+    };
+};
+
 jslibrary.directive('ngInitial', [ngInitialDirective]);
 jslibrary.directive('ngGotopath', [ngGotopathDirective]);
 jslibrary.directive('ngMessagemodel', [ngMessagemodelDirective]);
 
 jslibrary.service('$jsUrl', jsUrl);
+jslibrary.service('$jsTooltip', jsUrl);
